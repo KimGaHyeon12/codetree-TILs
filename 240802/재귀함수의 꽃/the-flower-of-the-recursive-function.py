b4d2a00e4ad38b5fdc76dddc13d69b8fb04e6d60
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2024-08-02 15:16:58 +0900

    Title: 재귀함수의 꽃, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/재귀함수의 꽃/the-flower-of-the-recursive-function.py
+++ 240802/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -1,14 +1,14 @@
 def print_num1(n):
     if n <= 0:
         return
-    print(n,' ',end='')
+    print(n,end='')
     print_num1(n-1)
 
 def print_num2(n):
     if n <= 0:
         return
     print_num2(n-1)
-    print(n,' ',end='')
+    print(n,end='')
 
 
 n=int(input())
