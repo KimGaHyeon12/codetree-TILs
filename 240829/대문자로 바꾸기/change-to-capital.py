f46df1802ff2d759d7427f3253a770bb637e39a2
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2024-08-29 16:31:46 +0900

    Title: 대문자로 바꾸기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240829/대문자로 바꾸기/change-to-capital.py
+++ 240829/대문자로 바꾸기/change-to-capital.py
@@ -3,5 +3,13 @@ for _ in range(5):
     arr.append(list(map(str,input().split())))
 
  
-s2 = [item.upper() for item in arr]  # Convert each string in the list to uppercase
-print(s2)
+ 
+new_arr= []
+for item  in arr:
+    temp = []
+    for i in item:
+        u = i.upper()
+        temp.append(u)
+    new_arr.append(temp)
+
+print(new_arr)
