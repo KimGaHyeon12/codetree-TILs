7a5a863da89302be852ecf4a87087cbff0727281
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2024-08-29 16:10:39 +0900

    Title: 두 숫자의 차의 최솟값, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 240829/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -3,7 +3,7 @@ arr =   list(map(int,input().split()))
  
 
 min_ = 200
-for i in range(1,5):
+for i in range(1,N):
         cha = arr[i] - arr[i-1]
         if cha>0 and cha<min_:
             min_=cha
