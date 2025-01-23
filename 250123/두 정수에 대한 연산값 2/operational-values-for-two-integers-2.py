d96d08fdc0faf420a79d0d2d34e4c47482af47ff
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-01-23 15:41:50 +0900

    Title: 두 정수에 대한 연산값 2, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250123/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 250123/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,6 +1,8 @@
 a, b = map(int, input().split())
 
 # Write your code here!
+org = [a,b]
+new = sorted(org)
 
 big =  max(a,b)
 small =  min(a,b)
@@ -8,4 +10,7 @@ small =  min(a,b)
 small +=10
 big = big *2
 
-print(small,big)
+if  org == new:
+    print(small,big)
+else:
+    print(big, small)
