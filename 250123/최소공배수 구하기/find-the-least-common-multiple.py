ffaf19d5c1efe8e4ef7fabeffba4c95bce9230f2
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-01-23 15:01:53 +0900

    Title: 최소공배수 구하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250123/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250123/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,14 +2,18 @@ n, m = map(int, input().split())
 
 # Write your code here!
 
-answer = -1
+answer = 1
 n_arr = []
 m_arr = []
-for i in range(1,max(n,m)):
+for i in range(1,max(n,m)+1):
     n_arr.append(n*i)
     m_arr.append(m*i)
 
 for n_ in n_arr:
     if n_ in m_arr:
-        print(n_)
+        answer = n_
         break
+
+#print(m_arr)
+#print( n_arr)
+print(answer)
