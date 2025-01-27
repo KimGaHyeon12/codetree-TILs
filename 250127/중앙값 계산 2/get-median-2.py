fe77f86d22b5f69f38c5c3e19b21daccf5b499bf
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-01-27 16:57:08 +0900

    Title: 중앙값 계산 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250127/중앙값 계산 2/get-median-2.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Write your code here!
+
+answers = []
+for i in range(1,n+1):
+    if i%2 != 0:
+        new_arr = arr[:i]
+        new_arr.sort()
+        #print(i, new_arr)
+        answers.append(new_arr[i//2])
+
+
+print(answers )
+
