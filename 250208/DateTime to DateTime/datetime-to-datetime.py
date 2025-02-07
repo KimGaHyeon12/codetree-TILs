e9c6c041b7fb86eaa0b15749ae29bb7191be2d75
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-02-08 04:20:49 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250208/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,26 @@
+a, b, c = map(int, input().split())
+
+# Write your code here!
+
+day, hour, mins = 11, 11, 11
+elapsed_time = 0
+
+while True:
+    if day == a and hour == b and mins == c:
+        break
+
+    elapsed_time += 1
+    mins += 1
+
+    if mins == 60:
+        hour += 1
+        mins = 0
+    if hour ==24:
+        day += 1
+        hour = 0
+
+
+if elapsed_time <= 0:
+    print(-1)
+else:
+    print(elapsed_time)
