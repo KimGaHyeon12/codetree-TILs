46cfd0b83284909aeb06f85498fde550bf78cf7b
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-03-19 17:59:10 +0900

    Title: 연속되는 수 2, Time: 75ms, Memory: 16MB, Status: Passed - CodeTree

--- 250319/연속되는 수 2/continuous-number2.py
+++ 250319/연속되는 수 2/continuous-number2.py
@@ -8,10 +8,12 @@ for i in range(n):
     if i == 0 or arr[i] == arr[i - 1]:
         cnt += 1
     else:
-        cnt_arr.append(cnt)
+        cnt_arr.append(cnt+1)
         cnt=0
+
+cnt_arr.append(cnt) #마지막 그룹은 반드시 별도로 추가해야 합니다.
 
 if not cnt_arr:
     print(1)
 else:
-    print(max(cnt_arr)+1)
+    print(max(cnt_arr))
