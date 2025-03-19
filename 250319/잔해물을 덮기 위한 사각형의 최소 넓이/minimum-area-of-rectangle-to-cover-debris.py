bd665bd3192c0e2d254cadc79a9746366e70be66
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-03-19 17:38:06 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 146ms, Memory: 50MB, Status: Runtime Error - CodeTree

---
+++ 250319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,36 @@
+x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
+x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+
+# Please write your code here.
+
+empty = [[0]*2002 for _ in range(2002)] #-1000
+
+for a in range(x1[0]+1000, x2[0]+1000):
+    for b in range(y1[0]+1000, y2[0]+1000):
+        empty[a][b]=1
+
+for a in range(x1[1]+1000, x2[1]+1000):
+    for b in range(y1[1]+1000, y2[1]+1000):
+        empty[a][b]=0
+
+x_arr = []
+y_arr = []
+for i in range(2001):
+    for j in range(2001):
+        if empty[i][j]==1:
+            x_arr.append(i)
+            y_arr.append(j)
+
+
+x_len = max(x_arr) - min(x_arr)  + 1
+y_len = max(y_arr) - min(y_arr) + 1
+
+#print(x_arr)
+#print(y_arr)
+
+#print(max(x_arr),  min(x_arr)  , x_len)
+#print(max(y_arr),  min(y_arr)  , y_len)
+
+answer = x_len * y_len
+print(answer)
