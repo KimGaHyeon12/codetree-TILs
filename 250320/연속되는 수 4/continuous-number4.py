c16c1fedda631d4a50d62b8700bf5f03ee920e14
kim ga hyeon <113492196+KimGaHyeon12@users.noreply.github.com>
2025-03-20 23:17:57 +0900

    Title: 연속되는 수 4, Time: 68ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250320/연속되는 수 4/continuous-number4.py
+++ 250320/연속되는 수 4/continuous-number4.py
@@ -2,21 +2,13 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-distance_remember = -999999
-count=0
-answer = []
-for i in range(1,n):
-    
-    if arr[i-2] + arr[i-1] == arr[i]:
-        count +=1
+ ans,cnt = 0,0
+
+ for i n range(n):
+    if i>=1 and arr[i]>arr[i-1]:
+        cnt+1
     else:
-        answer.append(count)
-        count=0
+        cnt=1
+    ans = max(ans,cnt)
 
-answer.append(count)
-
-if not answer:
-    print(1)
-else:
-    print(max(answer)+2)
-#print(answer)
+print(ans)
